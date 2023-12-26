b5ce743973e46050f0aff6d17de46de6f073e38e
Valeriy Kvashin <val_kv@mail.ru>
2023-12-26 19:38:44 +0200

    add main

---
+++ main.py
@@ -0,0 +1,7 @@
+from funcs import addition, subtraction
+
+result_add = addition(5, 3)
+print("Результат сложения:", result_add)
+
+result_sub = subtraction(10, 4)
+print("Результат вычитания:", result_sub)
