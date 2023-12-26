62a04bf849b59117e3c49fa994fc74375aebde61
Valeriy Kvashin <val_kv@mail.ru>
2023-12-26 19:38:30 +0200

    add funcs

---
+++ funcs.py
@@ -0,0 +1,25 @@
+def addition(a, b):
+    """
+    Функция для сложения двух чисел
+
+    Args:
+        a (int): Первое число
+        b (int): Второе число
+
+    Returns:
+        int: Результат сложения a и b
+    """
+    return a + b
+
+def subtraction(a, b):
+    """
+    Функция для вычитания двух чисел
+
+    Args:
+        a (int): Первое число
+        b (int): Второе число
+
+    Returns:
+        int: Результат вычитания b из a
+    """
+    return a - b
